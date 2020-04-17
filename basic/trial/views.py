56452e43369956cc825de2ab33cbeaf3f044382e
nepprk <nepprk@gmail.com>
2020-04-18 05:08:09 +0545

    code

--- basic/trial/views.py
+++ basic/trial/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def home(request):
+    #return HttpResponse("hello world")
+    return render(request,'home.html',{})
